485e6c367e6f5988b98d445cd5be86cd6a7467e7
tgautam03 <30533136+tgautam03@users.noreply.github.com>
2024-10-24 19:20:55 -0600

    FP32 Matrix datatype

---
+++ include/MatrixFP32.cuh
@@ -0,0 +1,29 @@
+#ifndef MATRIXFP32
+#define MATRIXFP32
+
+class MatrixFP32
+{
+public:
+    const int n_rows;        // Number of rows
+    const int n_cols;        // Number of cols
+
+    // Pointer to dynamic array
+    float* ptr;
+    
+    // Matrix in device memory: true; else: false
+    const bool on_device; 
+    
+    // Constructor to initialize n_rows x n_cols matrix
+    MatrixFP32(int n_rows_, int n_cols_, bool on_device);
+    
+    // Free memory
+    void free_mat();
+
+    // Copy to Device
+    void copy_to_device(MatrixFP32 d_mat);
+
+    // Copy to host
+    void copy_to_host(MatrixFP32 h_mat);
+};
+
+#endif
---
+++ src/MatrixFP32.cu
@@ -0,0 +1,50 @@
+#include <assert.h>
+#include <iostream>
+
+#include "../include/MatrixFP32.cuh"
+#include "../include/utils.cuh"
+
+MatrixFP32::MatrixFP32(int n_rows_, int n_cols_, bool on_device_) : n_rows(n_rows_), n_cols(n_cols_), on_device(on_device_)
+{
+    if (on_device_ == false)
+    {
+        // Initialize dynamic array
+        ptr = new float[n_rows*n_cols];
+    }
+    else
+    {
+        // Allocate device memory
+        cudaError_t err = cudaMalloc((void**) &ptr, n_rows*n_cols*sizeof(float));
+        cuda_check(err);
+    }
+}
+
+void MatrixFP32::free_mat()
+{
+    if (on_device == false)
+        delete[] ptr;
+    else
+        cudaFree(ptr);
+}
+
+void MatrixFP32::copy_to_device(MatrixFP32 d_mat)
+{
+    // Make sure that ptr is on host 
+    assert(on_device == false && "Matrix must be in host memory");
+    assert(d_mat.on_device == true && "Input Matrix to this function must be in device memory");
+
+    // Copying from host to device memory
+    cudaError_t err = cudaMemcpy(d_mat.ptr, ptr, n_rows*n_cols*sizeof(float), cudaMemcpyHostToDevice);
+    cuda_check(err);
+}
+
+void MatrixFP32::copy_to_host(MatrixFP32 h_mat)
+{
+    // Make sure that ptr is on device
+    assert(on_device == true && "Matrix must be in device memory");
+    assert(h_mat.on_device == false && "Input Matrix to this function must be in host memory");
+
+    // Copying from host to device memory
+    cudaError_t err = cudaMemcpy(h_mat.ptr, ptr, n_rows*n_cols*sizeof(float), cudaMemcpyDeviceToHost);
+    cuda_check(err);
+}
