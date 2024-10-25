f9a379968ed5a6dcccab14853f7412631c5930e5
tgautam03 <30533136+tgautam03@users.noreply.github.com>
2024-10-24 19:21:10 -0600

    Utility functions

---
+++ include/utils.cuh
@@ -0,0 +1,46 @@
+#ifndef UTILS
+#define UTILS
+#include <iostream>
+#include <assert.h>
+#include <random>
+#include <iomanip>
+#include <fstream>
+#include "../include/MatrixFP32.cuh"
+#include "../include/MatrixFP16.cuh"
+#include <cuda_fp16.h>
+
+// CUDA Error Checking
+#define cuda_check(err) { \
+    if (err != cudaSuccess) { \
+        std::cout << cudaGetErrorString(err) << " in " << __FILE__ << " at line " << __LINE__ << "\n"; \
+        exit(EXIT_FAILURE); \
+    } \
+}
+
+// CUBLAS Error Checking
+#define cublas_check(status) { \
+    if (status != CUBLAS_STATUS_SUCCESS) { \
+        std::cerr << "cuBLAS Error" << std::endl; \
+        exit(EXIT_FAILURE); \
+    } \
+}
+
+// Initalizing MatrixFP32 with val
+void init_mat(MatrixFP32 mat, float val);
+void init_mat(MatrixFP16 mat, half val);
+
+// Initalizing MatrixFP32 randomly between (MAX_VAL, MIN_VAL)
+void random_init_mat(MatrixFP32 mat, int MAX_VAL, int MIN_VAL);
+void random_init_mat(MatrixFP16 mat, int MAX_VAL, int MIN_VAL);
+
+// Print Matrix (Full or Partial)
+void print_mat(MatrixFP32 mat, bool full);
+
+// Asserting matrices are same within the tolerance (eps)
+void assert_mat(MatrixFP32 A_mat, MatrixFP32 B_mat, float eps);
+
+// Update benchmark.txt file with recorded times and GFLOPS
+void update_benckmark_txt(const std::string& filename, const double recorded_times[], 
+                        const double recorded_gflops[], const int mat_sizes[], 
+                        const int n_sizes);
+#endif
---
+++ src/utils.cu
@@ -0,0 +1,186 @@
+#include "../include/utils.cuh"
+
+void init_mat(MatrixFP32 mat, float val)
+{
+    // Getting Matrix Dimension
+    int n_rows = mat.n_rows; 
+    int n_cols = mat.n_cols;
+
+    // Initializing val to each location
+    for (int i = 0; i < n_rows; i++)
+    {
+        for (int j = 0; j < n_cols; j++)
+        {
+            mat.ptr[i*n_cols + j] = val;
+        }
+    }
+}
+
+void init_mat(MatrixFP16 mat, half val)
+{
+    // Getting Matrix Dimension
+    int n_rows = mat.n_rows; 
+    int n_cols = mat.n_cols;
+
+    // Initializing val to each location
+    for (int i = 0; i < n_rows; i++)
+    {
+        for (int j = 0; j < n_cols; j++)
+        {
+            mat.ptr[i*n_cols + j] = val;
+        }
+    }
+}
+
+void random_init_mat(MatrixFP32 mat, int MAX_VAL, int MIN_VAL)
+{
+    // Getting Matrix Dimension
+    int n_rows = mat.n_rows; 
+    int n_cols = mat.n_cols;
+
+    // Initializing val to each location
+    for (int i = 0; i < n_rows; i++)
+    {
+        for (int j = 0; j < n_cols; j++)
+        {
+            mat.ptr[i*n_cols + j] = (float)(rand() % (MAX_VAL - MIN_VAL + 1) + MIN_VAL);
+        }
+    }
+}
+
+void random_init_mat(MatrixFP16 mat, int MAX_VAL, int MIN_VAL)
+{
+    // Getting Matrix Dimension
+    int n_rows = mat.n_rows; 
+    int n_cols = mat.n_cols;
+
+    // Initializing val to each location
+    for (int i = 0; i < n_rows; i++)
+    {
+        for (int j = 0; j < n_cols; j++)
+        {
+            mat.ptr[i*n_cols + j] = (half)(rand() % (MAX_VAL - MIN_VAL + 1) + MIN_VAL);
+        }
+    }
+}
+
+void print_mat(MatrixFP32 mat, bool full)
+{
+    // Matrix must be on host
+    assert(mat.on_device == false && "For printing matrix must be on host");
+
+    // Getting Matrix Dimension
+    int n_rows = mat.n_rows; 
+    int n_cols = mat.n_cols;
+
+    if (full == true)
+    {
+        // Print Full Matrix
+        for (int i = 0; i < n_rows; i++)
+        {
+            std::cout << "| ";
+            for (int j = 0; j < n_cols; j++)
+            {
+                std::cout << std::setw(5) << std::to_string(mat.ptr[i*n_cols + j]) << " ";
+            }
+            std::cout << " |" << "\n";
+        }
+    }
+    else
+    {
+        // Print Partial Matrix
+        for (int i = 0; i < 5; i++)
+        {
+            std::cout << "| ";
+            for (int j = 0; j < 5; j++)
+            {
+                if (i == 3)
+                {
+                    std::cout << std::setw(5) << "  ...   " << " ";
+                }
+                else
+                {
+                    if (j != 3)
+                        std::cout << std::setw(5) << std::to_string(mat.ptr[i*n_cols + j]) << " ";
+                    else
+                        std::cout << std::setw(5) << "..." << " ";
+                }
+            }
+            std::cout << " |" << "\n";
+
+        }
+    }
+}
+
+void assert_mat(MatrixFP32 A_mat, MatrixFP32 B_mat, float eps)
+{
+    // Matrices must be on host
+    assert(A_mat.on_device == false && "For asserting matrix must be on host");
+    assert(B_mat.on_device == false && "For asserting matrix must be on host");
+
+    // Getting A Matrix Dimension
+    int A_n_rows = A_mat.n_rows; 
+    int A_n_cols = A_mat.n_cols;
+
+    // Getting B Matrix Dimension
+    int B_n_rows = B_mat.n_rows; 
+    int B_n_cols = B_mat.n_cols;
+
+    // Asserting that matrices have same dimensions
+    assert (A_n_rows == B_n_rows && "A rows must be equal to B rows");
+    assert (A_n_cols == B_n_cols && "A cols must be equal to B cols");
+
+    for (int i = 0; i < A_n_rows; i++)
+    {
+        for (int j = 0; j < A_n_cols; j++)
+        {
+            if (fabs(A_mat.ptr[i*A_n_cols + j] - B_mat.ptr[i*B_n_cols + j]) > eps)
+            {
+                std::cerr << "Assertion failed for " << "row number: " << i << ", col number: " << j << ".\n"
+                        << "Absolute Difference: " << fabs(A_mat.ptr[i*A_n_cols + j] - B_mat.ptr[i*B_n_cols + j]) << "\n";
+                assert(fabs(A_mat.ptr[i*A_n_cols + j] - B_mat.ptr[i*B_n_cols + j]) < eps && "Assertion failed!");
+            }
+        }
+    }
+}
+
+void update_benckmark_txt(const std::string& filename, const double recorded_times[], 
+                        const double recorded_gflops[], const int mat_sizes[], 
+                        const int n_sizes)
+{
+    // Opening File
+    std::ofstream file(filename);
+    if (!file.is_open()) 
+    {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return;
+    }
+
+    file << "Matrix Sizes" << ": ";
+    for (int i = 0; i < n_sizes; i++)
+    {
+        if (i != n_sizes-1)
+            file << mat_sizes[i] << " " ;
+        else
+            file << mat_sizes[i] << "\n \n" ;
+    }
+
+    file << "Time (Seconds)" << ": ";
+    for (int i = 0; i < n_sizes; i++)
+    {
+        if (i != n_sizes-1)
+            file << recorded_times[i] << " " ;
+        else
+            file << recorded_times[i] << "\n \n" ;
+    }
+
+    file << "GPLOPS" << ": ";
+    for (int i = 0; i < n_sizes; i++)
+    {
+        if (i != n_sizes-1)
+            file << recorded_gflops[i] << " " ;
+        else
+            file << recorded_gflops[i] << "\n \n" ;
+    }
+    
+}
